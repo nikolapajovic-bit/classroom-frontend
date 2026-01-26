0817fe9a065a69c5cc91ec6a24f9498a4039ce53
nikolapajovic-bit <nikpajovic@gmail.com>
2026-01-26 14:51:38 +0100

    implement class details page

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import SubjectsList from "./pages/subjects/list";
 import SubjectsCreate from "./pages/subjects/create";
 import ClassesList from './pages/classes/list';
 import ClassesCreate from './pages/classes/create';
+import ClassesShow from './pages/classes/show';
 
 function App() {
   return (
@@ -54,6 +55,7 @@ function App() {
                   name: 'classes',
                   list: '/classes',
                   create: '/classes/create',
+                  show: '/classes/show/:id',
                   meta: { label: 'Classes', icon: <GraduationCap /> }
                 }
               ]}
@@ -72,6 +74,7 @@ function App() {
                   <Route path="classes">
                     <Route index element={<ClassesList />} />
                     <Route path="create" element={<ClassesCreate />} />
+                    <Route path="show/:id" element={<ClassesShow />} />
                   </Route>
                 </Route>
               </Routes>
